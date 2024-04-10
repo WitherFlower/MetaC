da0f0afd94815373551585cbacd4d1f15e700252
MaximeBarniaudy <maxime.barniaudy@gmail.com>
2024-04-10 11:20:44 +0900

    On fait les tests comme on peut

--- grammar/test.sh
+++ grammar/test.sh
@@ -1,9 +1,5 @@
-leg -o grammar.c grammar.leg
-cc -o grammar grammar.c grammar_objects.c list.c -g
-(./grammar < grammar.leg) > thing
+meson compile thing -C ../builddir
+meson compile thing2 -C ../builddir
 
-cc -o test_get_tree test_get_tree.c grammar_objects.c grammar_get_tree.c list.c
-./test_get_tree > thing2
-
-diff thing thing2
+diff ../builddir/grammar/thing ../builddir/grammar/thing2
 
